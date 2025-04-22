187f326fbb8a5e8331f74ee031c6530105666f2c
sylviavitoria <sylvia.souza@dbserver.com.br>
2025-04-22 10:48:31 -0300

    chore: estrutura inicial do projeto

---
+++ src/test/java/com/sylviavitoria/arquiteturahexagonal/ArquiteturahexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sylviavitoria.arquiteturahexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArquiteturahexagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
